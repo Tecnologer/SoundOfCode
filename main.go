f1e9149aba1cb5b1897a08e542e273e515b6804a
Rey David <rdominguez@tecnologer.net>
2022-02-27 13:42:50 -0600

    std output samples

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ const (
 )
 
 var (
-	tau = math.Pi * 2
+	//π is Pi
+	π = math.Pi
+	//τ is tau (from Greek alphabet) constant for π*2
+	τ = π * 2
 )
 
 func main() {
@@ -25,14 +28,14 @@ func main() {
 	file := "out.bin"
 	f, _ := os.Create(file)
 	// sound := make([]byte, 0)
-	sound := generate(float32(2), float32(440))
+	sound := generate(float32(0.3), float32(440))
 	// for f := float32(27.5); f < 4186; f += 5 {
 	// 	sound = append(sound, generate(0.01, f)...)
 	// }
 
-	bw, _ := f.Write(sound)
-	fmt.Printf("\rWrote: %v bytes to %s", bw, file)
-	fmt.Fprintf(os.Stderr, "done")
+	_, _ = f.Write(sound)
+	// fmt.Printf("\rWrote: %v bytes to %s\n", bw, file)
+	// fmt.Fprintf(os.Stderr, "done")
 }
 
 func generate(duration, frequency float32) (sound []byte) {
@@ -42,13 +45,14 @@ func generate(duration, frequency float32) (sound []byte) {
 	// )
 	sound = make([]byte, 0)
 	nsamps := duration * SampleRate
-	var angle float64 = tau / float64(nsamps)
+	var angle float64 = τ / float64(nsamps)
 
 	// decayfac := math.Pow(end/start, 1.0/float64(nsamps))
 	for i := float32(0); i < nsamps; i++ {
 		sample := math.Sin(angle * float64(frequency) * float64(i))
 		// sample *= start
 		// start *= decayfac
+		fmt.Printf("%.8f\n", sample)
 		var buf [8]byte
 		binary.LittleEndian.PutUint32(buf[:],
 			math.Float32bits(float32(sample)))
